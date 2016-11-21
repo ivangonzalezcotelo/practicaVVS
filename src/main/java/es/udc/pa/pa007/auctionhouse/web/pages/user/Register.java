8e9093dc8f89519a0d5a052e86e1dba2ac46fa4f
Ivan Gonzalez Cotelo <ivan.gonzalez.cotelo@udc.es>
2016-11-21 17:08:26 +0100

    Primeras pruebas con CheckStyle, referencia a la issue #17

--- src/main/java/es/udc/pa/pa007/auctionhouse/web/pages/user/Register.java
+++ src/main/java/es/udc/pa/pa007/auctionhouse/web/pages/user/Register.java
@@ -18,78 +18,86 @@ import es.udc.pa.pa007.auctionhouse.web.services.AuthenticationPolicyType;
 import es.udc.pa.pa007.auctionhouse.web.util.UserSession;
 import es.udc.pojo.modelutil.exceptions.DuplicateInstanceException;
 
+/**
+ * Register controller
+ *
+ */
 @AuthenticationPolicy(AuthenticationPolicyType.NON_AUTHENTICATED_USERS)
 public class Register {
 
-    @Property
-    private String loginName;
+	@Property
+	private String loginName;
 
-    @Property
-    private String password;
+	@Property
+	private String password;
 
-    @Property
-    private String retypePassword;
+	@Property
+	private String retypePassword;
 
-    @Property
-    private String firstName;
+	@Property
+	private String firstName;
 
-    @Property
-    private String lastName;
+	@Property
+	private String lastName;
 
-    @Property
-    private String email;
+	@Property
+	private String email;
 
-    @SessionState(create=false)
-    private UserSession userSession;
+	@SessionState(create = false)
+	private UserSession userSession;
 
-    @Inject
-    private UserService userService;
+	@Inject
+	private UserService userService;
 
-    @Component
-    private Form registrationForm;
+	@Component
+	private Form registrationForm;
 
-    @Component(id = "loginName")
-    private TextField loginNameField;
+	@Component(id = "loginName")
+	private TextField loginNameField;
 
-    @Component(id = "password")
-    private PasswordField passwordField;
+	@Component(id = "password")
+	private PasswordField passwordField;
 
-    @Inject
-    private Messages messages;
+	@Inject
+	private Messages messages;
 
-    private Long userProfileId;
+	private Long userProfileId;
 
-    void onValidateFromRegistrationForm() {
+	/**
+	 * Validate the Register form
+	 */
+	void onValidateFromRegistrationForm() {
 
-        if (!registrationForm.isValid()) {
-            return;
-        }
+		if (!registrationForm.isValid()) {
+			return;
+		}
 
-        if (!password.equals(retypePassword)) {
-            registrationForm.recordError(passwordField, messages
-                    .get("error-passwordsDontMatch"));
-        } else {
+		if (!password.equals(retypePassword)) {
+			registrationForm.recordError(passwordField, messages.get("error-passwordsDontMatch"));
+		} else {
 
-            try {
-                UserProfile userProfile = userService.registerUser(loginName, password,
-                    new UserProfileDetails(firstName, lastName, email));
-                userProfileId = userProfile.getUserProfileId();
-            } catch (DuplicateInstanceException e) {
-                registrationForm.recordError(loginNameField, messages
-                        .get("error-loginNameAlreadyExists"));
-            }
+			try {
+				UserProfile userProfile = userService.registerUser(loginName, password,
+						new UserProfileDetails(firstName, lastName, email));
+				userProfileId = userProfile.getUserProfileId();
+			} catch (DuplicateInstanceException e) {
+				registrationForm.recordError(loginNameField, messages.get("error-loginNameAlreadyExists"));
+			}
 
-        }
+		}
 
-    }
+	}
 
-    Object onSuccess() {
+	/**
+	 * @return The index
+	 */
+	Object onSuccess() {
 
-        userSession = new UserSession();
-        userSession.setUserProfileId(userProfileId);
-        userSession.setFirstName(firstName);
-        return Index.class;
+		userSession = new UserSession();
+		userSession.setUserProfileId(userProfileId);
+		userSession.setFirstName(firstName);
+		return Index.class;
 
-    }
+	}
 
 }
